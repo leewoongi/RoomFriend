a2c72caca0137877da209dbdd0dad382e48a304b
leewoongi <t2korea@naver.com>
2020-10-23 18:57:45 +0900

    feat(#3) - Retrofit 싱글톤 생성 / Retrofit Api 생성

--- app/src/main/java/com/woon/roomfriends/repos/WebService.java
+++ app/src/main/java/com/woon/roomfriends/repos/WebService.java
@@ -1,5 +1,11 @@
 package com.woon.roomfriends.repos;
 
-public interface WebService {
+import com.woon.roomfriends.repos.entity.ResponseDao;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
+public interface WebService {
+    @GET("/search/users")
+    Call<ResponseDao> retrieveUser(@Query("q") String text);
 }
